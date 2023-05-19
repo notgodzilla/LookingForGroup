41f72794ef612d3416d97c9b1dfd4c043f4b5167
gail@notgodzilla.com <A9HGb6lmpp!!##>
2023-05-18 20:10:16 -0400

    Initial commit

---
+++ app/src/main/java/com/notgodzilla/lookingforgroup/PFListingsAdapter.kt
@@ -0,0 +1,32 @@
+package com.notgodzilla.lookingforgroup
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.notgodzilla.lookingforgroup.databinding.PfListingItemBinding
+
+class PFListingsAdapter(private val listings: List<String>) :
+    RecyclerView.Adapter<PFListingViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PFListingViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = PfListingItemBinding.inflate(inflater, parent, false)
+        return PFListingViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = listings.size
+
+    override fun onBindViewHolder(holder: PFListingViewHolder, position: Int) {
+        val item = listings[position]
+        holder.bind(item)
+    }
+}
+
+class PFListingViewHolder(private val binding: PfListingItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(text: String) {
+        binding.pfListingText.text = text
+
+    }
+
+}
---
+++ app/src/main/java/com/notgodzilla/lookingforgroup/PFListingsFragment.kt
@@ -0,0 +1,55 @@
+package com.notgodzilla.lookingforgroup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.notgodzilla.lookingforgroup.databinding.PfListingFragmentBinding
+import kotlinx.coroutines.launch
+
+class PFListingsFragment : Fragment() {
+
+    private var _binding: PfListingFragmentBinding? = null
+    private val binding
+        get() = checkNotNull(_binding)
+
+    private val viewModel: XIVPFViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = PfListingFragmentBinding.inflate(inflater, container, false)
+        binding.pfListingsRecycler.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.pfListingsRecycler
+            .addItemDecoration(
+                DividerItemDecoration(
+                    requireContext(),
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.listings.collect {
+                    binding.pfListingsRecycler.adapter = PFListingsAdapter(it)
+                }
+
+
+            }
+        }
+    }
+}
